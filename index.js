8dd13a11726f751a623b08e5c3ccd61605b72689
Juan Camilo Ospina <jospina@jospina-a01.vmware.com>
2022-01-03 16:03:42 -0500

    Docker compose test

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const redis = require('redis');
+const process = require('process');
+
+const app = express();
+const client = redis.createClient({
+  host: 'redis-server', // the name defined in the docker-compose file for the redis image
+  port: 6379,
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    res.send(`Number of visits is ${visits}`);
+    client.set('visits', parseInt(visits) + 1);
+  });
+});
+app.listen(8081, () => console.log('Listening on port 8081'));
